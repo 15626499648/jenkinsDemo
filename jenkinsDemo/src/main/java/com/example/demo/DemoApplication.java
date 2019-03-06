d7002b24ef7ea45f56ee4a748ef3dc07c2ea48ec
15626499648 <645359637@qq.com>
2019-03-06 16:02:21 +0800

    1

--- jenkinsDemo/src/main/java/com/example/demo/DemoApplication.java
+++ jenkinsDemo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,9 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+    @RequestMapping(value="/test")
+    public String testProject(){
+        return "test";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
