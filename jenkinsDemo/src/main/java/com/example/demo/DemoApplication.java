5e8eed958e4a39b3935f59f25d08dfde5eb54954
15626499648 <645359637@qq.com>
2019-03-07 17:42:46 +0800

    测试

--- jenkinsDemo/src/main/java/com/example/demo/DemoApplication.java
+++ jenkinsDemo/src/main/java/com/example/demo/DemoApplication.java
@@ -17,7 +17,7 @@ public class DemoApplication {
 
     @RequestMapping(value="/test1")
     public String testProject1(){
-        return "11111111111";
+        return "22222222222222";
     }
 
     public static void main(String[] args) {
