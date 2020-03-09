4af90ea216ac77e9d0a38f93140f92ec28993b98
15626499648 <645359637@qq.com>
2020-03-09 15:13:45 +0800

    测试OOM

--- jenkinsDemo/src/main/java/com/example/demo/DemoApplication.java
+++ jenkinsDemo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import com.example.demo.learn.Demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -17,7 +21,11 @@ public class DemoApplication {
 
     @RequestMapping(value="/test1")
     public String testProject1(){
-        return "2222";
+        List<Demo> demoList = new ArrayList<>();
+        while(true){
+            demoList.add(new Demo());
+        }
+
     }
 
     public static void main(String[] args) {
